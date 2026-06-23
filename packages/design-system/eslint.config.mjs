c94fad444bfde52e9dd41a1e47bd8c9c048afa91
hdg0116 <hdg0116@gmail.com>
2026-06-24 08:33:35 +0900

    chore(packages/design-system): DCMFE-27 Storybook VRT 환경 구성

--- packages/design-system/eslint.config.mjs
+++ packages/design-system/eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import config from '@dongchimi/eslint-config/react';
 
-export default config;
+export default [...config, ...storybook.configs['flat/recommended']];
