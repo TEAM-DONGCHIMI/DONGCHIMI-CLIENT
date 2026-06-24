1dcf8423a7169bc3f06aecc23458855b0431c18f
hdg0116 <hdg0116@gmail.com>
2026-06-24 16:40:32 +0900

    fix(design-system): 아이콘 생성 스크립트 실행 오류 수정 및 테스트용 아이콘 추가

--- packages/design-system/src/icons/index.ts
+++ packages/design-system/src/icons/index.ts
@@ -1,3 +1,3 @@
 // This barrel file is auto-generated. Do not edit it manually.
 // Run `pnpm icons:generate` to rebuild icon exports.
-export {};
+export { default as IcChevronRight } from './generated/IcChevronRight';
