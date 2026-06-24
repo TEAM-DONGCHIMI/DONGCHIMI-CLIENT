324c1d6c45f1da2d946ff2ed089e58a4a52fd6eb
hdg0116 <hdg0116@gmail.com>
2026-06-25 03:19:27 +0900

    chore(packages/design-system): DCMDS-27 Storybook props 문서화 설정 개선

--- packages/design-system/.storybook/preview.tsx
+++ packages/design-system/.storybook/preview.tsx
@@ -2,10 +2,20 @@ import type { Preview } from '@storybook/react-vite';
 
 const preview: Preview = {
   parameters: {
+    actions: {
+      argTypesRegex: '^on[A-Z].*',
+    },
     controls: {
+      expanded: true,
+      sort: 'requiredFirst',
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    docs: {
+      source: {
+        state: 'open',
       },
     },
 
