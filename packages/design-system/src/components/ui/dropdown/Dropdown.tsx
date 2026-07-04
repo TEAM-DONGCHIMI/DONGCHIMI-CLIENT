7669ee3c4d7ffd3e1557a76c49f04e48cc4da97d
Navi <masimello0001@gmail.com>
2026-07-05 04:40:08 +0900

    feat(packages/design-system): DCMDS-11 Dropdown.Item compound API 추가와 React 19 ref-prop 전환

--- packages/design-system/src/components/ui/dropdown/Dropdown.tsx
+++ packages/design-system/src/components/ui/dropdown/Dropdown.tsx
@@ -1,20 +1,31 @@
-import { forwardRef, type ComponentPropsWithoutRef, type ReactNode } from 'react';
+import type { ComponentPropsWithRef, ReactNode } from 'react';
 
 import { cn } from '../../../styles/class-name';
+import { ListButton } from '../list-button';
 import { dropdown } from './Dropdown.css';
 
-export interface DropdownProps extends ComponentPropsWithoutRef<'div'> {
+export interface DropdownProps extends ComponentPropsWithRef<'div'> {
   children: ReactNode;
 }
 
-export const Dropdown = forwardRef<HTMLDivElement, DropdownProps>(
-  ({ children, className, ...props }, ref) => {
-    return (
-      <div ref={ref} className={cn(dropdown, className)} {...props}>
-        {children}
-      </div>
-    );
-  },
-);
+const DropdownRoot = ({ children, className, ref, ...props }: DropdownProps) => {
+  return (
+    <div ref={ref} className={cn(dropdown, className)} {...props}>
+      {children}
+    </div>
+  );
+};
 
-Dropdown.displayName = 'Dropdown';
+DropdownRoot.displayName = 'Dropdown';
+
+export type DropdownItemProps = ComponentPropsWithRef<typeof ListButton>;
+
+const DropdownItem = ({ ref, ...props }: DropdownItemProps) => {
+  return <ListButton ref={ref} {...props} />;
+};
+
+DropdownItem.displayName = 'Dropdown.Item';
+
+export const Dropdown = Object.assign(DropdownRoot, {
+  Item: DropdownItem,
+});
