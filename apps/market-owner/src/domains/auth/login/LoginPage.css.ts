14d62c7a33be69c3df0ba015fcd09effaac1eb64
hdg0116 <hdg0116@gmail.com>
2026-07-08 00:27:24 +0900

    style(apps/market-owner): DCMSM-23 중복 스타일 제거

--- apps/market-owner/src/domains/auth/login/LoginPage.css.ts
+++ apps/market-owner/src/domains/auth/login/LoginPage.css.ts
@@ -9,10 +9,7 @@ export const cardClassName = style({
   minHeight: 'min(60.3rem, calc(100vh - min(3.2rem, 10vh)))',
   minWidth: 0,
   gap: 'clamp(2rem, 4vw, 3rem)',
-  borderRadius: '1.47rem',
   padding: 'clamp(2rem, 5vw, 4rem)',
-  backgroundColor: atomic.common[0],
-  boxShadow: '0 0 3rem rgb(23 23 23 / 5%)',
 });
 
 export const headerClassName = style({
