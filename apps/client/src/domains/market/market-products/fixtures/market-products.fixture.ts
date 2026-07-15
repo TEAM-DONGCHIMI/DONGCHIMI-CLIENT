23850ba31dc93d01e47751e78e3c33bf5bc29b9e
Navi <masimello0001@gmail.com>
2026-07-16 00:45:50 +0900

    feat(apps/client): DCMCL-21 오늘의 특가 화면 API 연결

--- apps/client/src/domains/market/market-products/fixtures/market-products.fixture.ts
+++ apps/client/src/domains/market/market-products/fixtures/market-products.fixture.ts
@@ -1,17 +1,3 @@
-export interface TodaySpecialProductFixtureTypes {
-  discountedPrice: number;
-  discountRate: number;
-  name: string;
-  originalPrice: number;
-  productId: number;
-  thumbnailUrl: string | null;
-}
-
-export interface TodaySpecialProductsFixtureTypes {
-  products: TodaySpecialProductFixtureTypes[];
-  totalCount: number;
-}
-
 export interface EventDiscountProductFixtureTypes {
   categoryId: string;
   discountedPrice: number;
@@ -36,83 +22,7 @@ export interface EventDiscountProductsFixtureTypes {
   pages: EventDiscountProductsPageFixtureTypes[];
 }
 
-export const DEFAULT_TODAY_SPECIAL_VISIBLE_COUNT = 2;
 export const DEFAULT_EVENT_CATEGORY_VISIBLE_COUNT = 2;
-
-const todaySpecialProducts = [
-  {
-    discountedPrice: 4500,
-    discountRate: 10,
-    name: '풀무원 콩나물 500g',
-    originalPrice: 5000,
-    productId: 201,
-    thumbnailUrl: null,
-  },
-  {
-    discountedPrice: 4500,
-    discountRate: 10,
-    name: '풀무원 콩나물 500g',
-    originalPrice: 5000,
-    productId: 202,
-    thumbnailUrl: null,
-  },
-  {
-    discountedPrice: 4500,
-    discountRate: 10,
-    name: '풀무원 콩나물 500g',
-    originalPrice: 5000,
-    productId: 203,
-    thumbnailUrl: null,
-  },
-  {
-    discountedPrice: 4500,
-    discountRate: 10,
-    name: '풀무원 콩나물 500g',
-    originalPrice: 5000,
-    productId: 204,
-    thumbnailUrl: null,
-  },
-  {
-    discountedPrice: 4500,
-    discountRate: 10,
-    name: '풀무원 콩나물 500g',
-    originalPrice: 5000,
-    productId: 205,
-    thumbnailUrl: null,
-  },
-  {
-    discountedPrice: 4500,
-    discountRate: 10,
-    name: '풀무원 콩나물 500g',
-    originalPrice: 5000,
-    productId: 206,
-    thumbnailUrl: null,
-  },
-  {
-    discountedPrice: 4500,
-    discountRate: 10,
-    name: '풀무원 콩나물 500g',
-    originalPrice: 5000,
-    productId: 207,
-    thumbnailUrl: null,
-  },
-  {
-    discountedPrice: 4500,
-    discountRate: 10,
-    name: '풀무원 콩나물 500g',
-    originalPrice: 5000,
-    productId: 208,
-    thumbnailUrl: null,
-  },
-  {
-    discountedPrice: 4500,
-    discountRate: 10,
-    name: '풀무원 콩나물 500g',
-    originalPrice: 5000,
-    productId: 209,
-    thumbnailUrl: null,
-  },
-] satisfies TodaySpecialProductFixtureTypes[];
 
 export const marketProductsFixture = {
   eventDiscount: {
@@ -295,8 +205,4 @@ export const marketProductsFixture = {
       },
     ] satisfies EventDiscountProductsPageFixtureTypes[],
   } satisfies EventDiscountProductsFixtureTypes,
-  todaySpecial: {
-    products: todaySpecialProducts,
-    totalCount: todaySpecialProducts.length,
-  } satisfies TodaySpecialProductsFixtureTypes,
 } as const;
